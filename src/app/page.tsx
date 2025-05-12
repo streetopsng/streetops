ee8e8566efec8ee4389c705ad34613a326a6d233
Victor Olusanmi-Sogbein <olusanmiv@gmail.com>
2025-05-12 12:14:30 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="">
-      <h1>Paige</h1>
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
