786d2f4dcfddb1317b810abbf953c7952d20feef
AdebestAA <aadebesta@gmail.com>
2025-05-13 21:21:21 +0100

    typography changes

--- src/components/LandingPageComponents/HeroSection.tsx
+++ src/components/LandingPageComponents/HeroSection.tsx
@@ -21,7 +21,7 @@ gsap.fromTo(headerEl.current,{
     })
     },[])
   return (
-    <div style={{zIndex:2}} className='relative lg:px-32 md:px-8 px-4 flex py-8 md:flex-row flex-col'>
+    <div style={{zIndex:2}} className='relative xl:px-32 md:px-8 px-4 flex py-8 md:flex-row flex-col'>
         {/* First Section */}
         <section className='px-2 md:w-[55%] w-full '>
 <span className='bg-[#2ABFD538] text-[#662AD5] inline-block px-4 rounded-xl text-[0.8rem] '>Powerful Platform</span>
--- src/components/LandingPageComponents/Section.tsx
+++ src/components/LandingPageComponents/Section.tsx
@@ -6,7 +6,7 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const Section = () => {
   return (
-    <div className="lg:px-32 md:px-8 px-4 flex flex-col justify-start items-center min-h-screen px-4  lg:px-20 py-10 space-y-10">
+    <div className="xl:px-32 md:px-8 px-4 flex flex-col justify-start items-center min-h-screen px-4  lg:px-20 py-10 space-y-10">
       {/* Header Section */}
       {/* <div className="text-center space-y-4 max-w-2xl">
         <h1 className="font-semibold text-2xl sm:text-3xl lg:text-[2.7rem] leading-tight">
@@ -33,13 +33,14 @@ const Section = () => {
             Balanced Workforces.
           </h2>
           <p>
-          We are championing workforce evolution, offering innovative operational strategies that drive business transformation. By integrating specialized recruitment, smart automations and workflow optimization, we empower organizations to thrive. Our focus is on delivering 'The Future of Balanced Workforces' - where talent blends with technology. <></>
+          We are championing workforce evolution, offering innovative operational strategies that drive business transformation. By integrating specialized recruitment, smart automations and workflow optimization, we empower organizations to thrive. Our focus is on delivering <q>The Future of Balanced Workforces</q> - where talent blends with technology. <></>
             {/* <br className="hidden sm:block" />
             payments to suppliers. This type of financing is also known as <></>
             <br className="hidden sm:block" />
             supply chain financing and provides businesses with an <></>
             <br className="hidden sm:block" />
             efficient solution. */}
+           
           </p>
           <Link href="/#" className="text-primary flex items-center gap-2">
             Get Started <FaArrowRight />
@@ -52,17 +53,17 @@ const Section = () => {
       <div className="flex flex-col lg:flex-row gap-6 w-full max-w-6xl">
         {/* People */}
         <div className="flex-1 bg-purple-100 p-6 rounded-lg flex flex-col items-center">
-          <h2 className="font-semibold text-xl lg:text-2xl text-center">
+          <h2 className="uppercase font-semibold text-xl lg:text-2xl text-center">
           People
           </h2>
-          <p className="text-base pt-4 text-justify">
+          <p className="text-base pt-4 ">
         We believe that people are the core of every successful organization. Our People section provides strategic solutions to maximize your human capital investment. We offer:
-              <br />
-        Strategic Recruitment: Identifying and placing the right talent to achieve your business objectives.
         <br />
-        Transformative Training: Equipping your workforce with the skills and knowledge needed to excel.
+        <strong>Strategic Recruitment</strong> : Identifying and placing the right talent to achieve your business objectives.
         <br />
-        Human Capital Leadership (Africa): Actively contributing to the development of human capital across the African continent through webinars, training programs, and a dynamic blog, fostering a community of growth and innovation.
+        <strong>Transformative Training</strong> : Equipping your workforce with the skills and knowledge needed to excel.
+        <br />
+        <strong>Human Capital Leadership (Africa)</strong>: Actively contributing to the development of human capital across the African continent through webinars, training programs, and a dynamic blog, fostering a community of growth and innovation.
           </p>
           <Image
             alt="paige"
@@ -75,15 +76,18 @@ const Section = () => {
 
         {/* Processes */}
         <div className="flex-1 bg-purple-200 p-6 rounded-lg flex flex-col items-center">
-          <h2 className="font-semibold text-xl lg:text-2xl text-center">
+          <h2 className=" uppercase font-semibold text-xl lg:text-2xl text-center">
            Processes
           </h2>
-          <p className="text-base pt-4 text-justify">
+          <p className="text-base pt-4">
           We partner with organizations to transform their operations and achieve peak performance. Our Processes section delivers strategic consulting services, including:
-              <br />
-          Operational Consulting: Providing expert guidance to optimize workflows, reduce costs, and improve overall operational effectiveness.
-          Process Automation: Leveraging automation technologies to drive efficiency, accuracy, and scalability.
-          Comprehensive Consulting Solutions: Offering a suite of consulting services to support organizational change, improve project delivery, and enhance overall business performance.
+<br />
+          <strong>Operational Consulting</strong> : Providing expert guidance to optimize workflows, reduce costs, and improve overall operational effectiveness.
+          <br />
+          
+          <strong>Process Automation</strong> : Leveraging automation technologies to drive efficiency, accuracy, and scalability.
+          <br />
+          <strong>Comprehensive Consulting Solutions</strong> : Offering a suite of consulting services to support organizational change, improve project delivery, and enhance overall business performance.
           </p>
           <Image
             alt="paige"
@@ -95,9 +99,9 @@ const Section = () => {
         </div>
       </div>
       {/* Get many advanatages section */}
-      <div className="flex flex-col-reverse lg:flex-row items-center mt-10 pt-10 p-6 rounded-lg gap-6 w-full max-w-6xl">
+      {/* <div className="flex flex-col-reverse lg:flex-row items-center mt-10 pt-10 p-6 rounded-lg gap-6 w-full max-w-6xl"> */}
   {/* Text Section */}
-  <div className="flex-1 space-y-4">
+  {/* <div className="flex-1 space-y-4">
     <h2 className="font-semibold text-2xl lg:text-[2.2rem] leading-snug">
       Get many advantages when <></>
       <br className="block sm:hidden" /><></>
@@ -115,11 +119,11 @@ const Section = () => {
     <button type="submit" className="bg-primary text-white p-2 rounded-lg hover:bg-wht border-primary transition-all duration-500 hover:text-primary cursor-pointer">
       Read More
     </button>
-  </div>
+  </div> */}
 
   {/* Image Section */}
-  <Image alt="paige" src="/solution-1.png" width={300} height={300} />
-</div>
+  {/* <Image alt="paige" src="/solution-1.png" width={300} height={300} /> */}
+{/* </div> */}
 
     </div>
   );
