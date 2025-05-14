e733f2ed1a51a0fb330dd8a0f4b3f8cb11046079
AdebestAA <aadebesta@gmail.com>
2025-05-14 13:27:45 +0100

    remove font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-bgcolor font-poppins`}
+        className={`bg-bgcolor `}
       >
         {children}
       </body>
