b24a206510fa790ce4ecd4b656fe63fe3e1931fa
AdebestAA <aadebesta@gmail.com>
2025-07-03 07:07:51 +0100

    little changes again

--- src/AdminComponents/PostJobsComponent/FormForJobPosting.tsx
+++ src/AdminComponents/PostJobsComponent/FormForJobPosting.tsx
@@ -59,7 +59,7 @@ const postData = async(formDetails:formDetailsType<string>)=>{
         alert("Empty field")
         return
     }
-    const res = await fetch(`${address}/api/post-jobs/`,{
+    const res = await fetch(`/api/post-jobs/`,{
         method:"POST",
         headers:{
             "Content-Type":"application/json"
